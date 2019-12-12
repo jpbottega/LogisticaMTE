5cc868a7f55d3c9b1e424a6fcc0b82e158d5a5b3
juan <juanpbottega@gmail.com>
2019-12-12 01:13:19 -0300

    ultimo

--- Logistica/settings.py
+++ Logistica/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'abdcdcans'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'abdcdcans'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
